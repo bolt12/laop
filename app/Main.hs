62d60189f002372db25ffa1c1c1977699e81bf3b
Armando Santos <armandoifsantos@gmail.com>
2019-12-28 23:08:46 +0000

    Small fixes

--- app/Main.hs
+++ app/Main.hs
@@ -39,9 +39,33 @@ mulSSM3 = fromF' (mulSS (Nat 3))
 die :: Dist (Natural 6) 6
 die = uniform [nat @6 1 .. nat 6]
 
+-- Sprinkler
+
+rain :: Dist Bool 2
+rain = choose 0.8
+
+sprinkler :: Matrix Double 2 2
+sprinkler = fromLists [[0.6, 0.99], [0.4, 0.01]]
+
+grass :: Matrix Double 4 2
+grass = fromLists [[1, 0.2, 0.1, 0.01], [0, 0.8, 0.9, 0.99]]
+
+state = khatri grass identity `comp` khatri sprinkler identity `comp` rain
+
+grass_wet = row [0,1] `comp` kp1 @Double @2 @4
+
+rainning = row [0,1] `comp` kp2 @Double @2 @2 `comp` kp2 @Double @2 @4
+
 main :: IO ()
 main = do 
+    putStrLn "Monty Hall Problem solution:"
     prettyPrint (p1 @Double @1 `comp` secondChoice `comp` firstChoice)
+    putStrLn "\n Sum of dices probability:"
     prettyPrint (sumSSM `comp` khatri die die)
+    putStrLn "\n Checking that the last result is indeed a distribution: "
     prettyPrint (bang `comp` sumSSM `comp` khatri die die)
+    putStrLn "\n Probability of grass being wet:"
+    prettyPrint (grass_wet `comp` state)
+    putStrLn "\n Probability of rain:"
+    prettyPrint (rainning `comp` state)
 
--- src/Matrix/Type.hs
+++ src/Matrix/Type.hs
@@ -166,20 +166,26 @@ instance Eq e => Eq (Matrix e cols rows) where
 
 instance Num e => Num (Matrix e cols rows) where
 
-  Empty + Empty             = Empty
-  (One a) + (One b)         = One (a + b)
-  (Junc a b) + (Junc c d)   = Junc (a + c) (b + d)
-  (Split a b) + (Split c d) = Split (a + c) (b + d)
+  Empty + Empty                = Empty
+  (One a) + (One b)            = One (a + b)
+  (Junc a b) + (Junc c d)      = Junc (a + c) (b + d)
+  (Split a b) + (Split c d)    = Split (a + c) (b + d)
+  x@(Split a b) + y@(Junc c d) = x + abideJS y
+  x@(Junc a b) + y@(Split c d) = abideJS x + y
 
   Empty - Empty             = Empty
   (One a) - (One b)         = One (a - b)
   (Junc a b) - (Junc c d)   = Junc (a - c) (b - d)
   (Split a b) - (Split c d) = Split (a - c) (b - d)
+  x@(Split a b) - y@(Junc c d) = x - abideJS y
+  x@(Junc a b) - y@(Split c d) = abideJS x - y
 
   Empty * Empty             = Empty
   (One a) * (One b)         = One (a * b)
   (Junc a b) * (Junc c d)   = Junc (a * c) (b * d)
   (Split a b) * (Split c d) = Split (a * c) (b * d)
+  x@(Split a b) * y@(Junc c d) = x * abideJS y
+  x@(Junc a b) * y@(Split c d) = abideJS x * y
 
   abs Empty       = Empty
   abs (One a)     = One (abs a)
