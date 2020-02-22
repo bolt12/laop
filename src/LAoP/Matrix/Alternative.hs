584863927d1a898aeb41507dd11d044ab1539f0e
Andrey Mokhov <andrey.mokhov@gmail.com>
2020-02-22 16:48:16 +0000

    Refactor the data type and add Khatri-Rao product

--- src/LAoP/Matrix/Alternative.hs
+++ src/LAoP/Matrix/Alternative.hs
@@ -28,28 +28,51 @@ import Prelude hiding (id, (.), fst, snd, curry, uncurry)
 
 import qualified Prelude
 
+
 data Matrix e a b where
     Identity :: Matrix e a a
     Zero     :: Matrix e a b
-    Scale    :: e -> Matrix e a b -> Matrix e a b
-    Add      :: Matrix e a b -> Matrix e a b -> Matrix e a b
+    Lift     :: (e -> e -> e) -> Matrix e a b -> Matrix e a b -> Matrix e a b
     Join     :: Matrix e a c -> Matrix e b c -> Matrix e (Either a b) c
     Fork     :: Matrix e a b -> Matrix e a c -> Matrix e a (Either b c)
 
-instance Category (Matrix e) where
+empty :: Matrix e Void Void
+empty = Identity
+
+emap :: (e -> e) -> Matrix e a b -> Matrix e a b
+emap f = Lift (const f) Zero
+
+scale :: Num e => e -> Matrix e a b -> Matrix e a b
+scale e = emap (e*)
+
+one :: Num e => e -> Matrix e () ()
+one e = scale e Identity
+
+constant :: e -> Matrix e a b
+constant e = emap (const e) Zero
+
+-- Better switch to using a custom type class Field without the signum nonsense
+instance Num e => Num (Matrix e a b) where
+    fromInteger = constant . fromInteger
+    (+)         = Lift (+)
+    (-)         = Lift (-)
+    (*)         = Lift (*)
+    abs         = emap abs
+    negate      = emap negate
+    signum      = error "No sensible definition"
+
+instance Num e => Category (Matrix e) where
     id = Identity
 
-    Identity  . x         = x
-    x         . Identity  = x
-    Zero      . _         = Zero
-    _         . Zero      = Zero
-    Scale x y . z         = Scale x (y . z)
-    x         . Scale y z = Scale y (x . z)
-    Add x y   . z         = Add (x . z) (y . z)
-    x         . Add y z   = Add (x . y) (x . z)
-    Join w x  . Fork y z  = Add (w . y) (x . z)
-    Fork x y  . z         = Fork (x . z) (y . z)
-    x         . Join y z  = Join (x . y) (x . z)
+    Identity   . x          = x
+    x          . Identity   = x
+    Zero       . _          = Zero
+    _          . Zero       = Zero
+    Lift f x y . z          = Lift f (x . z) (y . z)
+    x          . Lift f y z = Lift f (x . y) (x . z)
+    Join w x   . Fork y z   = (w . y) + (x . z)
+    Fork x y   . z          = Fork (x . z) (y . z)
+    x          . Join y z   = Join (x . y) (x . z)
 
 -- Adapted from https://hackage.haskell.org/package/categories
 class Category k => Cartesian k where
@@ -64,7 +87,7 @@ instance Cartesian (->) where
     snd = Prelude.snd
     (f &&& g) a = (f a, g a)
 
-instance Cartesian (Matrix e) where
+instance Num e => Cartesian (Matrix e) where
     type Product (Matrix e) = Either
     fst   = Join Identity Zero
     snd   = Join Zero Identity
@@ -75,7 +98,7 @@ bimapProduct :: Cartesian k => k a c -> k b d -> Product k a b `k` Product k c d
 bimapProduct f g = (f . fst) &&& (g . snd)
 
 -- For free!
-(><) :: Matrix e a b -> Matrix e c d -> Matrix e (Either a c) (Either b d)
+(><) :: Num e => Matrix e a b -> Matrix e c d -> Matrix e (Either a c) (Either b d)
 (><) = bimapProduct
 
 infixl 4 ><
@@ -91,7 +114,7 @@ bimapSum :: CoCartesian k => k a c -> k b d -> Sum k a b `k` Sum k c d
 bimapSum f g = (inl . f) ||| (inr . g)
 
 -- For free!
-(-|-) :: Matrix e a b -> Matrix e c d -> Matrix e (Either a c) (Either b d)
+(-|-) :: Num e => Matrix e a b -> Matrix e c d -> Matrix e (Either a c) (Either b d)
 (-|-) = bimapSum
 
 infixl 5 -|-
@@ -103,7 +126,7 @@ instance CoCartesian (->) where
     (f ||| _) (Left  a) = f a
     (_ ||| g) (Right a) = g a
 
-instance CoCartesian (Matrix e) where
+instance Num e => CoCartesian (Matrix e) where
     type Sum (Matrix e) = Either
     inl = Fork Identity Zero
     inr = Fork Zero Identity
@@ -116,23 +139,19 @@ instance Distributive (->) where
     distribute (a, Left  b) = Left  (a, b)
     distribute (a, Right c) = Right (a, c)
 
-instance Distributive (Matrix e) where
+instance Num e => Distributive (Matrix e) where
     distribute = Fork (id -|- fst) (id -|- snd)
-
-empty :: Matrix e Void Void
-empty = Identity
-
-one :: e -> Matrix e () ()
-one e = Scale e Identity
 
 transpose :: Matrix e a b -> Matrix e b a
 transpose m = case m of
-    Zero      -> Zero
-    Identity  -> Identity
-    Scale x y -> Scale x (transpose y)
-    Add x y   -> Add (transpose x) (transpose y)
-    Join x y  -> Fork (transpose x) (transpose y)
-    Fork x y  -> Join (transpose x) (transpose y)
+    Identity   -> Identity
+    Zero       -> Zero
+    Lift f x y -> Lift f (transpose x) (transpose y)
+    Join x y   -> Fork (transpose x) (transpose y)
+    Fork x y   -> Join (transpose x) (transpose y)
 
-select :: Matrix e a (Either b c) -> Matrix e b c -> Matrix e a c
+select :: Num e => Matrix e a (Either b c) -> Matrix e b c -> Matrix e a c
 select x y = Join y id . x
+
+kr :: Num e => Matrix e a b -> Matrix e a c -> Matrix e a (Either b c)
+kr a b = (transpose fst . a) * (transpose snd . b)
